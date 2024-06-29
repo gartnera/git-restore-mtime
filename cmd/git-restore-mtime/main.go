499c36919e055895f7c3bc822dd5cb950ffa405e
Alex Gartner <git@agartner.com>
2024-06-29 15:12:50 -0700

    Optimize filesystem open (#2)

--- cmd/git-restore-mtime/main.go
+++ cmd/git-restore-mtime/main.go
@@ -2,24 +2,44 @@ package main
 
 import (
 	"os"
+	"runtime/pprof"
 
 	gitrestoremtime "github.com/gartnera/git-restore-mtime"
 	"github.com/spf13/cobra"
 )
 
 const (
-	maxDepthArg = "max-depth"
+	maxDepthArg   = "max-depth"
+	cpuProfileArg = "cpu-profile"
 )
 
 func init() {
 	flags := rootCmd.Flags()
 	flags.Int(maxDepthArg, 0, "maximum depth to traverse the commit history (default unlimited)")
+
+	persistentFlags := rootCmd.PersistentFlags()
+	persistentFlags.String(cpuProfileArg, "", "path to write pprof cpu profile")
 }
 
 var rootCmd = &cobra.Command{
 	Use:          "git-restore-mtime <path>",
 	Args:         cobra.ExactArgs(1),
 	SilenceUsage: true,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		cpuProfile, _ := cmd.Flags().GetString(cpuProfileArg)
+		if cpuProfile != "" {
+			f, err := os.Create(cpuProfile)
+			if err != nil {
+				return err
+			}
+			return pprof.StartCPUProfile(f)
+		}
+		return nil
+	},
+	PersistentPostRunE: func(cmd *cobra.Command, args []string) error {
+		pprof.StopCPUProfile()
+		return nil
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		opts := []gitrestoremtime.ManagerOptionT{}
 		if maxDepth, _ := cmd.Flags().GetInt(maxDepthArg); maxDepth > 0 {
